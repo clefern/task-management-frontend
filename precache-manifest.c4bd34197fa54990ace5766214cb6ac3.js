7a60abfacdee9649560974ad53bc0e80a5281bd0
clefern <clefern@users.noreply.github.com>
2022-08-28 00:35:18 +0000

    deploy: 91136a173e79ffddb8e403fef054e8ca34a71e01

---
+++ precache-manifest.c4bd34197fa54990ace5766214cb6ac3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0950d179b61946d3d386529f965f8b05",
+    "url": "https://clefern.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "8e89de892d9f10f2426f",
+    "url": "https://clefern.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "572c62756613a3a317cd",
+    "url": "https://clefern.github.io/task-management-frontend/static/js/2.14fc34c8.chunk.js"
+  },
+  {
+    "revision": "8e89de892d9f10f2426f",
+    "url": "https://clefern.github.io/task-management-frontend/static/js/main.f8702923.chunk.js"
+  },
+  {
+    "revision": "a501224d1e3db779faf7",
+    "url": "https://clefern.github.io/task-management-frontend/static/js/runtime~main.eed85437.js"
+  }
+]);
